ae4902d1a251c120e573b5714cb8b9c50e90ca69
annaju23 <annajulianascimentosiqueira11@gmail.com>
2025-09-29 21:57:21 -0300

    Feat: Criado os modelos para o banco de dados

---
+++ salaoBeleza.Desktop/Banco de Dados/SalaoBelezaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using salaoBeleza.Desktop.Modelos;
+
+namespace salaoBeleza.Desktop.Banco_de_Dados;
+
+internal class SalaoBelezaContext:DbContext
+{
+    public DbSet<Usuario> Usuarios { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string textoConexao = "server=localhost; user=root; password=; database=salaobeleza";
+        optionsBuilder.UseMySql(textoConexao, ServerVersion.AutoDetect(textoConexao));
+    }
+}
---
+++ salaoBeleza.Desktop/Modelos/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salaoBeleza.Desktop.Modelos;
+
+internal class Usuario
+{
+    public int id { get; set; }
+    public string NomeCompleto { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+}
