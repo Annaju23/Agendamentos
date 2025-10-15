f8bafc108e770b1869f3a19e55a5a9cd9bf1dfd1
annaju23 <annajulianascimentosiqueira11@gmail.com>
2025-10-14 21:59:33 -0300

    Feat: adiciona controlador de cliente//2Commit

---
+++ Agendamentos.API/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using Agendamentos.Biblioteca;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agendamentos.API.Controllers;
+
+[Route("[controller]")] // Anotação para definir a rota do controlador
+[ApiController]
+public class ClientController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult RegisterClient([FromBody]Client request)
+    {
+        return Created();
+    }
+
+    [HttpGet("{id}")] 
+    public IActionResult GetClientByID(int id)
+    {
+        return Ok(new
+        {
+            Message = id
+        });
+    }
+}
