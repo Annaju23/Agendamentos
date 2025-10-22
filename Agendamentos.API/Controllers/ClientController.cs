394752e11d0127877ae2a05886223f210e518f1b
annaju23 <annajulianascimentosiqueira11@gmail.com>
2025-10-21 21:07:32 -0300

    FEAT : Completa o controlador de cliente

--- Agendamentos.API/Controllers/ClientController.cs
+++ Agendamentos.API/Controllers/ClientController.cs
@@ -7,30 +7,30 @@ namespace Agendamentos.API.Controllers;
 
 [Route("[controller]")] // Anotação para definir a rota do controlador
 [ApiController]
-public class ClientController(APIContext context): ControllerBase
+public class ClientController(APIContext context) : ControllerBase
 {
     private APIContext _context = context;
     [HttpPost]
-    public async Task<IActionResult> RegisterClientAsync([FromBody]Client request)
+    public async Task<IActionResult> RegisterClientAsync([FromBody] Client request)
     {
         await _context.Clients.AddAsync(request);
         _context.SaveChangesAsync();
         return CreatedAtAction(nameof(GetClientByIDAsync), new { id = request.ID });
     }
 
-    [HttpGet("{id}")] 
-    public async Task <IActionResult> GetClientByIDAsync(int id)
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetClientByIDAsync(int id)
     {
         //Vai procurar o cliente pelo id aqui
         // _context.Clients.Find()
         //Se não tiver usuario 404 Not Found
         Client? result = await _context.Clients.FindAsync(id);
         if (result is null) return NotFound("Cliente não foi encontrado!");
-        return Ok(result);  
+        return Ok(result);
 
     }
     [HttpPut]
-    public async Task <IActionResult> UpdateClientAsync([FromBody] Client request)
+    public async Task<IActionResult> UpdateClientAsync([FromBody] Client request)
     {
         if (request.ID == null) return BadRequest();
         Client? result = await _context.Clients.FindAsync(request.ID);
@@ -41,8 +41,33 @@ public class ClientController(APIContext context): ControllerBase
         return Ok();
 
     }
-     // Crie os metodos abaixo com melhor retorno aplicavel
+    // Crie os metodos abaixo com melhor retorno aplicavel
     //HTTP PUT 200 OK | 204 no content UpdateClientAsync()
     //HTTP PATCH 204 no content | 200 OK ToggleEmailFromClientIdAsync
     //DELETE 204 no content DeleteClienteByIdAsync()
+
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> ToggleEmailFromClientIdAsync (int Id)
+    {
+        Client? result = await _context.Clients.FindAsync(Id);
+        if (result is null) return NotFound("Cliente não deseja receber email!");
+        result.CanReceiveEmail = !result.CanReceiveEmail;
+        await _context.SaveChangesAsync();
+        return NoContent();
+
+    }
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> DeleteClienteByIdAsync(int Id)
+    {
+        Client? client = await _context.Clients.FindAsync(Id);
+        if (client == null) return NotFound();
+        _context.Remove(client);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+
+
+
 }
